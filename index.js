593111a3df4cec6e8e7af4ef308ca98212237b0e
Daniela Ramirez <Daniela.ramirez003@gmail.com>
2018-01-15 18:46:02 -0300

    actualizacion

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const axios = require('axios')
 
-axios.get('http://localhost:3000/estado')
+setInterval(function(){ axios.get('http://localhost:3000/estado')
   .then(function (response) {
     console.log('res', response.data);
   })
   .catch(function (error) {
     console.log('err', error);
   });
+ }, 3000);
