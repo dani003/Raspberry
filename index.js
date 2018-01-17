f5b706a7a7c15f50ffba7df1ca7630c717c565aa
Daniela Ramirez <Daniela.ramirez003@gmail.com>
2018-01-17 11:32:33 -0300

    Actualizacion Puerto 2002

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const axios = require('axios')
 
-setInterval(function(){ axios.get('http://localhost:3000/estado')
+setInterval(function(){ axios.get('http://pird.ddns.net:2002/estado')
   .then(function (response) {
     console.log('res', response.data);
   })
   .catch(function (error) {
     console.log('err', error);
   });
- }, 3000);
+}, 3000);
